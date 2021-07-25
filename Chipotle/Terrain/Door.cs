d6a4032619681471ca4e7bb819556655dadc2454
lvamos <lukas.vamos@gmail.com>
2021-07-25 12:31:53 +0200

    * refactor: make Door.OnUseobject protected

--- Chipotle/Terrain/Door.cs
+++ Chipotle/Terrain/Door.cs
@@ -32,7 +32,7 @@ namespace Game.Terrain
             Closed = closed;
         }
 
-        private void OnUseObject(UseObject m)
+        protected  void OnUseObject(UseObject m)
         {
             if (Closed)
                 Open(m.Tile.Position);
