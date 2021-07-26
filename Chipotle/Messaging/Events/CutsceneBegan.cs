f02a7b2c4540ec5bb2c4a6348402294342c71027
Lukáš Vámoš <lukas.vamos@gmail.com>
2021-07-26 16:29:53 +0200

    * refactor!: add detailed information about concerning cutscene to CutsceneBegan and CutsceneEnded
    Add name of played cutscene and handle of corresponding sound object to the classes and their constructor.

--- Chipotle/Messaging/Events/CutsceneBegan.cs
+++ Chipotle/Messaging/Events/CutsceneBegan.cs
@@ -2,15 +2,19 @@
 {
     public class CutsceneBegan : GameMessage
     {
-
+        public readonly  string CutsceneName;
+        public readonly int SoundID;
 
         /// <summary>
         /// Constructs instance of the message.
         /// </summary>
         /// <param name="sender">Source of the message</param>
         /// <param name="soundName">Identifier of the audio cut scene file</param>
-        public CutsceneBegan(object sender) : base(sender)
-        { }
+        public CutsceneBegan(object sender, string cutsceneName, int soundID) : base(sender)
+        {
+            CutsceneName = cutsceneName;
+            SoundID = soundID;
+        }
 
     }
 }
--- Chipotle/Messaging/Events/CutsceneEnded.cs
+++ Chipotle/Messaging/Events/CutsceneEnded.cs
@@ -2,14 +2,19 @@
 {
     public class CutsceneEnded : GameMessage
     {
+        public readonly string CutsceneName;
+        public readonly int SoundID;
 
         /// <summary>
         /// Constructs instance of the message.
         /// </summary>
         /// <param name="sender">Source of the message</param>
         /// <param name="soundName">Identifier of the audio cut scene file</param>
-        public CutsceneEnded(object sender) : base(sender)
-        { }
+        public CutsceneEnded(object sender, string cutsceneName, int soundID) : base(sender)
+        {
+            CutsceneName = cutsceneName;
+            SoundID = soundID;
+        }
 
     }
 }
