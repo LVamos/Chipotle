66cb62bf1613df08255e4fc3136219981ab288ed
Lukáš Vámoš <lukas.vamos@gmail.com>
2021-07-26 17:27:09 +0200

    * chore: add new command message (GotoPoint)

---
+++ Chipotle/Messaging/Commands/GotoPoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Luky;
+
+using System;
+using System.Collections.Generic;
+
+namespace Game.Messaging.Commands
+{
+    public class GotoPoint : GameMessage
+{
+    public readonly Queue<Vector2> Path;
+    public readonly int StepLength;
+
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="sender">Creator of the message</param>
+    /// <param name="path">List of points of the path to the target</param>
+    /// <param name="stepLength">Length of one step in milliseconds</param>
+    public GotoPoint(object sender, Queue<Vector2> path, int stepLength) : base(sender)
+    {
+        Path = path ?? throw new ArgumentNullException(nameof(path));
+        StepLength = stepLength;
+    }
+}
+}
