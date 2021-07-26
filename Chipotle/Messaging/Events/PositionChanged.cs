8cebdb480841dd26f9784e7091ee7b9068443510
Lukáš Vámoš <lukas.vamos@gmail.com>
2021-07-26 18:00:52 +0200

    * chore: add new event message (PositionChanged)
    This event is intended to be used only inside entities.

---
+++ Chipotle/Messaging/Events/PositionChanged.cs
@@ -0,0 +1,12 @@
+﻿using Game.Terrain;
+
+namespace Game.Messaging.Events
+{
+    public class PositionChanged
+    {
+        public readonly Plane Area;
+
+        public PositionChanged(object sender, Plane area) : base(sender)
+            => Area = new Plane(area);
+    }
+}
