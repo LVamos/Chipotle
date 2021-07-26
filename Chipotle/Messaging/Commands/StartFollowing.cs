43881dc76e8bac5b6dd7a2465ea807a1077236f3
Lukáš Vámoš <lukas.vamos@gmail.com>
2021-07-26 23:42:38 +0200

    * chore: add two new commands (startFollowing and StopFollowing)

---
+++ Chipotle/Messaging/Commands/StartFollowing.cs
@@ -0,0 +1,8 @@
+﻿namespace Game.Messaging.Commands
+{
+    public class StartFollowing : GameMessage
+    {
+        public StartFollowing(object sender) : base(sender)
+        { }
+    }
+}
---
+++ Chipotle/Messaging/Commands/StopFollowing.cs
@@ -0,0 +1,8 @@
+﻿namespace Game.Messaging.Commands
+{
+    public class StopFollowing : GameMessage
+    {
+        public StopFollowing(object sender) : base(sender)
+        { }
+    }
+}
