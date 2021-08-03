2a87d4574b708f3d16b5d2a349c44799f9d68f19
Lukáš Vámoš <lukas.vamos@gmail.com>
2021-08-03 19:37:02 +0200

    * feat: add untested version of Chipotlee's car.

--- Chipotle/Entities/ChipotlesCar.cs
+++ Chipotle/Entities/ChipotlesCar.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using Game.Messaging.Commands;
 using Game.Terrain;
+using System.Collections.Generic;
+using System.Linq;
 
 using Luky;
 
@@ -9,6 +11,8 @@ namespace Game.Entities
 {
     public class ChipotlesCar: DumpObject
     {
+        protected HashSet<Locality> _destinations = new HashSet<Locality>();
+
         protected HashSet<Locality> _visitedLocalities = new HashSet<Locality>();
         public IReadOnlyCollection<Locality> VisitedLocalities
         {
@@ -22,9 +26,47 @@ namespace Game.Entities
         {
             base.OnUseObject(message);
 
-            if(_visitedLocalities.IsNullOrEmpty())
-                _actionSoundID = World.Sound.Play(stream: World.Sound.GetRandomSoundStream("snd14"), null, false, PositionType.Absolute, message.Tile.Position.AsOpenALVector(), true, 1f, null, 1f, 0, Playback.OpenAL);
+            // Local function to check if an object from Walsh area was used
+            bool Used(string name)
+                => World.GetObject(name).Used;
 
+            // Check if all localities of Walsh area were visited.
+            bool walshAreaExplored =
+            World.Player.VisitedLocalities.Count == 14
+                    && World.Player.VisitedLocalities.All(l => l.Name.Indexed.ToLower().Contains("w1"));
+
+            // Check if some important objects were used
+            bool ObjectsUsed=
+                 (new string[]
+                 {"tělo w1", "hadice w1", "popelnice w1", "prkno w1", "lavička w3"})
+            .All(o => Used(o));
+
+            bool leftArea = !_visitedLocalities.IsNullOrEmpty(); // Checks if player left Walsh area with the car
+
+            if(!leftArea && !(ObjectsUsed && walshAreaExplored))
+            {
+                _actionSoundID = World.Sound.Play(
+                    World.Sound.GetRandomSoundStream("snd14"), 
+                    null, 
+                    false, 
+                    PositionType.Absolute, 
+                    message.Tile.Position.AsOpenALVector(), 
+                    true, 
+                    1f, 
+                    null, 
+                    1f, 
+                    0, 
+                    Playback.OpenAL
+                    );
+                return;
+            }
+
+            // If player didn't leave Walsh area but used required objects and went through all area
+            if(!leftArea && ObjectsUsed && walshAreaExplored)
+            {
+                _destinations.Add(World.GetLocality("ulice p1"));
+                World.PlayCutscene(this, "cs20");
+            }
         }
     }
 }
