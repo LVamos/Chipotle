1410aa98992739b9e6d6dc0d4427d40e0990a534
Lukáš Vámoš <lukas.vamos@gmail.com>
2021-08-08 15:27:41 +0200

    * feat: implement pub tables

---
+++ Chipotle/Entities/PubTable.cs
@@ -0,0 +1,34 @@
+﻿using Game.Messaging.Commands;
+using Game.Terrain;
+
+using Luky;
+
+
+namespace Game.Entities
+{
+    public class PubTable : DumpObject
+    {
+        public PubTable(Name name, Plane area) : base(name, area, "hospodský stůl")
+        { }
+
+        protected override void OnUseObject(UseObject message)
+        {
+            Entity tuttle = World.GetEntity("tuttle");
+
+            if (
+                !World.GetLocality("ulice h1").IsItHere(tuttle)
+                && !Locality.IsItHere(tuttle)
+                )
+            {
+                _cutscene = "cs24";
+            }
+            else
+            {
+                _cutscene = "cs25";
+            }
+
+            base.OnUseObject(message);
+        }
+
+    }
+}
