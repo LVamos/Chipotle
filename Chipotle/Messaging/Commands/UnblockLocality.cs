fcf6235eba2b1255d038f14096799b89fa8c07d7
Lukáš Vámoš <lukas.vamos@gmail.com>
2021-08-13 23:44:40 +0200

    * feat: add UnblockLocality message
    This message is dedicated to ChipotlesCar. And it means that it can be visited by the car.

---
+++ Chipotle/Messaging/Commands/UnblockLocality.cs
@@ -0,0 +1,12 @@
+﻿using Game.Terrain;
+
+
+namespace Game.Messaging.Commands
+{
+    public class UnblockLocality : GameMessage
+    {
+        public readonly Locality Locality;
+
+        public UnblockLocality(object sender, Locality locality) : base(sender) => Locality = locality;
+    }
+}
