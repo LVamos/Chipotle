c878b3e0e9912b257a8e73c8734d982e7314307e
Lukáš Vámoš <lukas.vamos@gmail.com>
2021-09-09 17:28:22 +0200

    *fix: fix unblocking "ulice v1" locality

--- Chipotle/Entities/CarsonsBench.cs
+++ Chipotle/Entities/CarsonsBench.cs
@@ -1,5 +1,7 @@
-﻿using Game.Messaging.Commands;
+﻿using Game.Messaging;
+using Game.Messaging.Commands;
 using Game.Terrain;
+using Game.Entities;
 
 using Luky;
 
@@ -12,6 +14,8 @@ namespace Game.Entities
         public CarsonsBench(Name name, Plane area) : base(name, area, "lavice u Carsona", null, null, null, "cs32", true)
         { }
 
+        private ChipotlesCar Car  => World.GetObject("detektivovo auto") as ChipotlesCar;
+
         protected override void OnUseObject(UseObject message)
         {
             if (
@@ -20,6 +24,7 @@ namespace Game.Entities
                 )
             {
                 base.OnUseObject(message);
+                Car.ReceiveMessage(new UnblockLocality(this, World.GetLocality("ulice v1")));
             }
         }
     }
