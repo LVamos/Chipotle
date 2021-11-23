4b5a71ba910d9454774b346d49bda829ac983439
lvamos <lukas.vamos@gmail.com>
2021-11-23 22:55:39 +0100

    *feat: add fading effect

--- Sound/Sound/FadingRecord.cs
+++ Sound/Sound/FadingRecord.cs
@@ -9,33 +9,12 @@ namespace Luky
     /// <summary>
     /// Describes how a sound's volume should be dynamically modified.
     /// </summary>
-    public struct FadingRecord
+    public class FadingRecord
     {
-        /// <summary>
-        /// Declares types of fading.
-        /// </summary>
-        public enum FadingType
-        {
-            /// <summary>
-            /// Specifies that a sound should be fluently muted.
-            /// </summary>
-            In,
-
-            /// <summary>
-            /// specifies that a sound should be fluently unmuted.
-            /// </summary>
-            Out
-        };
-
         /// <summary>
         /// Method of fading for the specified sound
         /// </summary>
         public readonly FadingType Type;
-
-        /// <summary>
-        /// Identifier of the sound to be modified.
-        /// </summary>
-        public readonly Sound Sound;
 
         /// <summary>
         /// specifies how steep should the volume change be.
@@ -50,13 +29,11 @@ namespace Luky
         /// <summary>
         /// Constructor
         /// </summary>
-        /// <param name="sound">Identifier of the sound to be modified.</param>
         /// <param name="type">Method of fading for the specified sound</param>
         /// <param name="volumeDelta">specifies how steep should the volume change be.</param>
         /// <param name="targetVolume">Specifies final volume of the specified sound.</param>
-        public FadingRecord(Sound sound, FadingType type, float volumeDelta, float targetVolume)
+        public FadingRecord(FadingType type, float volumeDelta, float targetVolume)
         {
-            Sound = sound;
             Type = type;
             TargetVolume = targetVolume;
             VolumeDelta = volumeDelta;
