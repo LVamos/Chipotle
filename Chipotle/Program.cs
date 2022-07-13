70bcc77dc14ec92d3787f62112ab8c93a2a83b1d
Lukáš Vámoš <lukas.vamos@gmail.com>
2022-07-13 18:40:36 +0200

    *chore: add a change that defines various program modes

--- Chipotle/Program.cs
+++ Chipotle/Program.cs
@@ -29,67 +29,134 @@ namespace Game
             /// <summary>
             /// Specifies if the JAWS key hook should be disabled.
             /// </summary>
-            public static bool DisableJawsKeyHook = false;
+            public static bool DisableJawsKeyHook;
 
             /// <summary>
             /// Enables some test methods.
             /// </summary>
-            public static bool TestCommandsEnabled = true;
+            public static bool TestCommandsEnabled;
 
             /// <summary>
             /// Enables or disables cutscenes.
             /// </summary>
-            public static bool PlayCutscenes = false;
+            public static bool PlayCutscenes;
 
             /// <summary>
             /// Custom initial position for Tuttle
             /// </summary>
-            public static Vector2? TuttleTestStart = new Vector2(1070, 1058);
+            public static Vector2? TuttleTestStart;
 
             /// <summary>
             /// Enables or disables a custom initial posiiton for Tuttle.
             /// </summary>
-            public static bool AllowTuttlesCustomPosition = false;
+            public static bool AllowTuttlesCustomPosition;
 
             /// <summary>
             /// Enables or disables a custom initial position for Chipotle.
             /// </summary>
-            public static bool AllowCustomChipotlesStartPosition = false;
+            public static bool AllowCustomChipotlesStartPosition;
+			
+            /// <summary>
+            /// Enables or disables commands for loading and creating predefined saves.
+            /// </summary>
+            public static bool AllowPredefinedSaves;
 
             /// <summary>
             /// Enables or disables error reporting.
             /// </summary>
-            public static bool ReportErrors = false;
+            public static bool ReportErrors;
 
             /// <summary>
             /// Enables or disables raising unhandled exceptions.
             /// </summary>
-            public static bool ThrowExceptions = true;
+            public static bool ThrowExceptions;
 
             /// <summary>
             /// Enables or disables music in main menu.
             /// </summary>
-            public static bool PlayMenuLoop = false;
+            public static bool PlayMenuLoop;
 
             /// <summary>
             /// Enables or disables main menu at startup.
             /// </summary>
-            public static bool MainMenuAtStartup = false;
+            public static bool MainMenuAtStartup;
 
             /// <summary>
             /// Enables or disables game saving in each locality and after each interaction with a object or door.
             /// </summary>
-            public static bool SaveGameInEachLocality = false;
+            public static bool SaveGameInEachLocality;
 
             /// <summary>
             /// Enables or disables sending Tuttle to the pool locality at startup.
             /// </summary>
-            public static bool SendTuttleToPool = true;
+            public static bool SendTuttleToPool;
 
             /// <summary>
             /// Enables or disables Tuttle's Chipotle following.
             /// </summary>
-            public static bool LetTuttleFollowChipotle = true;
+            public static bool LetTuttleFollowChipotle;
+
+			/// <summary>
+			/// Switches the game to debug mode.
+			/// </summary>
+			public static void SetDebugMode()
+			{
+				AllowTuttlesCustomPosition = false;
+				AllowCustomChipotlesStartPosition = false;
+                AllowPredefinedSaves = true;
+				DisableJawsKeyHook = false;
+				LetTuttleFollowChipotle = true;
+				MainMenuAtStartup = true;
+				PlayCutscenes = true;
+				PlayMenuLoop = true;
+				ReportErrors = false;
+				SaveGameInEachLocality = true;
+				SendTuttleToPool = true;
+				TestCommandsEnabled = true;
+				ThrowExceptions = true;
+			}
+
+			/// <summary>
+			/// Prepares the game for release.
+			/// </summary>
+			public static void SetReleaseMode()
+			{
+				AllowTuttlesCustomPosition = false;
+				AllowCustomChipotlesStartPosition = false;
+                AllowPredefinedSaves = false;
+				DisableJawsKeyHook = true;
+				LetTuttleFollowChipotle = true;
+				MainMenuAtStartup = true;
+				PlayCutscenes = true;
+				PlayMenuLoop = true;
+				ReportErrors = true;
+				SaveGameInEachLocality = false;
+				SendTuttleToPool = true;
+				TestCommandsEnabled = false;
+				ThrowExceptions = false;
+			}
+
+			/// <summary>
+			/// Prepares the game for testing purposes.
+			/// </summary>
+			public static void SetTestMode()
+            {
+                AllowTuttlesCustomPosition = false;
+                AllowCustomChipotlesStartPosition = false;
+                AllowPredefinedSaves = true;
+				DisableJawsKeyHook = true;
+                LetTuttleFollowChipotle = true;
+                MainMenuAtStartup = true;
+                PlayCutscenes = true;
+                PlayMenuLoop = true;
+                ReportErrors = true;
+				SaveGameInEachLocality = true;
+                SendTuttleToPool = true;
+				TestCommandsEnabled = false;
+				ThrowExceptions = false;
+			}
+			
+
         }
 
 
@@ -291,6 +358,7 @@ namespace Game
         private static void Main()
         {
             ResendErrorReport();
+            Settings.SetDebugMode();
             Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             SerializationPath = Path.Combine(DataPath, "game.sav");
