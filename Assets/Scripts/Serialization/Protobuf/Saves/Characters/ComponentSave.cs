dfa3bdf84588eaa82496f3ddddfb448827a246b1
Lukáš Vámoš <lukas.vamos@gmail.com>
2026-03-16 16:21:39 +0100

    *feat: add missing attributes to serialization data objects

--- Assets/Scripts/Serialization/Protobuf/Saves/Characters/ComponentSave.cs
+++ Assets/Scripts/Serialization/Protobuf/Saves/Characters/ComponentSave.cs
@@ -5,6 +5,9 @@ using ProtoBuf;
 namespace Game.Serialization.Protobuf.Snapshots.Characters
 {
 	[ProtoContract(SkipConstructor = true, ImplicitFields = ImplicitFields.AllFields)]
+	[ProtoInclude(300, typeof(AISave))]
+	[ProtoInclude(301, typeof(PhysicsSave))]
+	[ProtoInclude(302, typeof(SoundSave))]
 	public class ComponentSave : EntitySave
 	{
 		public string Owner { get; set; }
